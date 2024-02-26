7642f69d8b9be30940d8231f0819af83629b407c
제일 <joimh7@gmail.com>
2024-02-26 15:21:55 +0900

    reduce 테스트

---
+++ reduce.js
@@ -0,0 +1,19 @@
+//accumulator 누적값, currentValue 현재값, index는 인덱스, arr은 요소
+
+const array = [ 6, 2, 1, 8, 10 ]
+
+const result = array.reduce((
+    accumulator,
+    currentValue,
+    index,
+    arr
+) => {
+  console.log(accumulator, currentValue, index);
+  return acc + cur;
+}, 0);
+// 0 6 0
+// 6 2 1
+// 8 1 2
+// 9 8 3
+// 17 10 4
+result; // 27
